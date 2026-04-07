956643e7b98f690ccee38632ed736aa5d42d0061
Yamlak Negash <164669390+yamneg96@users.noreply.github.com>
2026-04-07 09:27:25 +0300

    Update db.js
    added timeout

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // The time the driver will wait for a server selection before timing out
+      serverSelectionTimeoutMS: 30000, 
+      // The time the driver will wait for a connection to be established
+      connectTimeoutMS: 30000,
+    });
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
